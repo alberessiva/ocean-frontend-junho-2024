cc79a8c96cebb7bdef2abafccd962a27267f388b
Alberes Silva <43136324+alberes-silva@users.noreply.github.com>
2024-06-06 14:59:47 +0000

    Estilização do card

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
     return (
     <>
-      <div>
+      <div className="card">
         <h2>Game of Thrones</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/pt/a/a4/Game_of_Thrones_Temporada_1_Poster.jpg"/>
+        <img src="https://m.media-amazon.com/images/S/pv-target-images/d89a8295f39a4a9509a497d13d3b6dfb24c61bc0c4d5d30111330ad2fd2e4dd6._BR-6_AC_SX360_FMwebp_.jpg"/>
       </div>
     </>
   )

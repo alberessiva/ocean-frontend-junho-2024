b9a1f1f7cbda6dd9841f92463a29aa834f5c7ecf
Alberes Silva <43136324+alberes-silva@users.noreply.github.com>
2024-06-06 14:31:11 +0000

    Bloco criado

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
     return (
     <>
-      App
+      <div>
+        <h2>Game of Thrones</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/pt/a/a4/Game_of_Thrones_Temporada_1_Poster.jpg"/>
+      </div>
     </>
   )
 }
